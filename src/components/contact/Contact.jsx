2bc90a0dba678e0770c3dc424bf97bb4fd4a197d
Samya brata Maji <samyabratamaji334@gmail.com>
2023-02-28 01:24:48 +0530

    added refs

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import { toast, ToastContainer } from "react-toastify";
 import emailjs from "@emailjs/browser";
 
@@ -6,110 +6,115 @@ import "./Contact.css";
 import "react-toastify/dist/ReactToastify.css";
 
 const Contact = (props) => {
-    const [name, setName] = useState("");
-    const [email, setEmail] = useState("");
-    const [subject, setSubject] = useState("");
-    const [message, setMessage] = useState("");
-    const [loading, setLoading] = useState(false);
+  const nameRef = useRef();
+  const emailRef = useRef();
+  const subjectRef = useRef();
+  const messageRef = useRef();
 
-    const submitHandler = async (e) => {
-        e.preventDefault();
-        if (!name || !email || !subject || !message) {
-            return toast.error("Please complete the form above");
-        }
+  const [loading, setLoading] = useState(false);
 
-        setLoading(true);
+  const submitHandler = async (e) => {
+    e.preventDefault();
+    if (
+      !nameRef.current.value ||
+      !emailRef.current.value ||
+      !subjectRef.current.value ||
+      !messageRef.current.value
+    ) {
+      return toast.error("Please complete the form above");
+    }
 
-        const data = {
-            name,
-            email,
-            subject,
-            message,
-            to_email: 'bimalkumarsaha06@gmail.com'
-        };
+    setLoading(true);
 
-        emailjs
-            .send(
-                process.env.REACT_APP_EMAILJS_SERVICE_ID,
-                process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
-                data,
-                process.env.REACT_APP_EMAILJS_PUBLIC_API
-            )
-            .then(
-                (result) => {
-                    setLoading(false);
-                    toast.success(`Successfully sent email.`);
-                },
-                (error) => {
-                    setLoading(false);
-                    console.log(error);
-                    toast.error(error.text);
-                }
-            );
+    const data = {
+      name: nameRef.current.value,
+      email: emailRef.current.value,
+      subject: subjectRef.current.value,
+      message: messageRef.current.value,
+      to_email: "bimalkumarsaha06@gmail.com",
     };
 
-    return (
-        <section className="contact container section" id="contact">
-            <h2 className="section__title">Get In Touch</h2>
+    emailjs
+      .send(
+        process.env.REACT_APP_EMAILJS_SERVICE_ID,
+        process.env.REACT_APP_EMAILJS_TEMPLATE_ID,
+        data,
+        process.env.REACT_APP_EMAILJS_PUBLIC_API
+      )
+      .then(() => {
+        setLoading(false);
+        toast.success(`Successfully sent email.`);
+      })
+      .catch((error) => {
+        // todo: add error handler
+        setLoading(false);
+        // console.log(error);
+        // toast.error(error.text);
+      });
+  };
 
-            <div className="contact__container grid">
-                <div className="contact__info">
-                    <h3 className="contact__title">Let's talk about everything!</h3>
-                    <p className="contact__details">
-                        Don't like forms? Send me an email. 👋
-                    </p>
-                </div>
+  return (
+    <section className="contact container section" id="contact">
+      <h2 className="section__title">Get In Touch</h2>
 
-                <form onSubmit={submitHandler} className="contact__form">
-                    <div className="contact__form-group">
-                        <div className="contact__form-div">
-                            <input
-                                type="text"
-                                className="contact__form-input"
-                                placeholder="Insert your name"
-                                onChange={(e) => setName(e.target.value)}
-                            />
-                        </div>
+      <div className="contact__container grid">
+        <div className="contact__info">
+          <h3 className="contact__title">Let's talk about everything!</h3>
+          <p className="contact__details">
+            Don't like forms? Send me an email. 👋
+          </p>
+        </div>
 
-                        <div className="contact__form-div">
-                            <input
-                                type="email"
-                                className="contact__form-input"
-                                placeholder="Insert your email"
-                                onChange={(e) => setEmail(e.target.value)}
-                            />
-                        </div>
-                    </div>
-
-                    <div className="contact__form-div">
-                        <input
-                            type="text"
-                            className="contact__form-input"
-                            placeholder="Insert your subject"
-                            onChange={(e) => setSubject(e.target.value)}
-                        />
-                    </div>
-
-                    <div className="contact__form-div contact__form-area">
-                        <textarea
-                            name=""
-                            id=""
-                            cols="30"
-                            rows="10"
-                            className="contact__form-input"
-                            placeholder="Write your message"
-                            onChange={(e) => setMessage(e.target.value)}
-                        ></textarea>
-                    </div>
-
-                    <button type="submit" className="btn">
-                        {loading ? "Sending..." : "Send Message"}
-                    </button>
-                </form>
-                <ToastContainer position="bottom-right" theme={props.theme} />
+        <form onSubmit={submitHandler} className="contact__form">
+          <div className="contact__form-group">
+            <div className="contact__form-div">
+              <input
+                type="text"
+                className="contact__form-input"
+                placeholder="Insert your name"
+                ref={nameRef}
+              />
             </div>
-        </section>
-    );
+
+            <div className="contact__form-div">
+              <input
+                type="email"
+                className="contact__form-input"
+                placeholder="Insert your email"
+                ref={emailRef}
+              />
+            </div>
+          </div>
+
+          <div className="contact__form-div">
+            <input
+              type="text"
+              className="contact__form-input"
+              placeholder="Insert your subject"
+              ref={subjectRef}
+            />
+          </div>
+
+          <div className="contact__form-div contact__form-area">
+            <textarea
+              name=""
+              id=""
+              cols="30"
+              rows="10"
+              className="contact__form-input"
+              placeholder="Write your message"
+              ref={messageRef}
+            ></textarea>
+          </div>
+
+          <button type="submit" className="btn">
+            {loading ? "Sending..." : "Send Message"}
+          </button>
+        </form>
+        <ToastContainer position="bottom-right" theme={props.theme} />
+      </div>
+    </section>
+  );
 };
 
 export default Contact;
