8ff5ec9cc648136c2fc7804668153f4017ff7bb0
PAWAN KUMAR JHA <88606641+Pjha72@users.noreply.github.com>
2023-05-13 18:25:55 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -17,7 +17,30 @@ const Home = () => {
 
                 <HeaderSocials />
 
-                <a href="#contact" className="btn"> Contact Me</a>
+                <a href="#contact" > 
+                        <button class="blob-btn">
+                            Contact Me
+                            <span class="blob-btn__inner">
+                            <span class="blob-btn__blobs">
+                                <span class="blob-btn__blob"></span>
+                                <span class="blob-btn__blob"></span>
+                                <span class="blob-btn__blob"></span>
+                                <span class="blob-btn__blob"></span>
+                            </span>
+                            </span>
+                        </button>
+                    <br/>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
+                    <defs>
+                        <filter id="goo">
+                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
+                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
+                        <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
+                        </filter>
+                    </defs>
+                    </svg>
+                </a>
 
                 <ScrollDown />
             </div>
