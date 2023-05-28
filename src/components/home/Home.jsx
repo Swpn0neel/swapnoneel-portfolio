4c971d7551dcc23190055face7607fc6b4950768
Swapnoneel Saha <121167506+Swpn0neel@users.noreply.github.com>
2023-05-28 21:46:58 +0530

    Update Home.jsx

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -30,16 +30,6 @@ const Home = () => {
                             </span>
                         </button>
                     <br/>
-
-                    <svg xmlns="http://www.w3.org/2000/svg" version="1.1">
-                    <defs>
-                        <filter id="goo">
-                        <feGaussianBlur in="SourceGraphic" result="blur" stdDeviation="10"></feGaussianBlur>
-                        <feColorMatrix in="blur" mode="matrix" values="1 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 21 -7" result="goo"></feColorMatrix>
-                        <feBlend in2="goo" in="SourceGraphic" result="mix"></feBlend>
-                        </filter>
-                    </defs>
-                    </svg>
                 </a>
 
                 <ScrollDown />
